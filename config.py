8b185b6a99ae02f4383c3ed22a335d7ebefab1ce
Dmitriy Medvedev <medvedevdmitry21@gmail.com>
2023-12-07 12:44:18 +0300

    Add database credentials to config

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ from os import getenv
 load_dotenv("./venv/.env")
 
 TOKEN = getenv("TOKEN")
+
+MYSQL_USER = getenv("MYSQL_USER")
+MYSQL_PASSWORD = getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = getenv("MYSQL_DATABASE")
+MYSQL_HOST = getenv("MYSQL_HOST")
+MYSQL_PORT = getenv("MYSQL_PORT")
